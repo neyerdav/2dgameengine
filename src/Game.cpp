dce6f442405a726fd68135d8bbee2f44ec7de8b4
David Neyer <neyer.david@googlemail.com>
2020-05-08 16:47:10 +0200

    add projectile

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "./Constants.h"
 #include "./Game.h"
+#include "../lib/glm/glm.hpp"
 
 Game::Game() {
     this -> isRunning = false;
@@ -52,21 +53,36 @@ void Game::ProcessInput() {
     }
 }
 
-void Game::Update() {
+glm::vec2 projectilePos = glm::vec2(0.0f, 0.0f);
+glm::vec2 projectileVel = glm::vec2(20.0f, 20.0f);
 
+void Game::Update() {
+    int timeToWait = FRAME_TARGET_TIME - (SDL_GetTicks() - ticksLastFrame);
+    if (timeToWait > 0 && timeToWait <= FRAME_TARGET_TIME ) {
+        SDL_Delay(timeToWait);
+    }
+
+    float deltaTime = (SDL_GetTicks() - ticksLastFrame) / 1000.0f;
+    deltaTime = (deltaTime > 0.05f) ? 0.05f : deltaTime;
+    ticksLastFrame = SDL_GetTicks();
+
+   projectilePos = glm::vec2(
+       projectilePos.x + projectileVel.x * deltaTime,
+       projectilePos.y + projectileVel.y * deltaTime
+   );
 }
 
 void Game::Render() {
     SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
     SDL_RenderClear(renderer);
-    SDL_Rect rectangle {
-        (int) PosX,
-        (int) PosY,
+    SDL_Rect projectile {
+        (int) projectilePos.x,
+        (int) projectilePos.y,
         10,
         10
     };
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderFillRect(renderer, &rectangle);
+    SDL_RenderFillRect(renderer, &projectile);
     SDL_RenderPresent(renderer);
 }
 
