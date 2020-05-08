a0657bea812e08fda74572746958e40395d9774b
David Neyer <neyer.david@googlemail.com>
2020-05-08 14:12:13 +0200

    basic game loop

---
+++ src/Game.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include "./Constants.h"
+#include "./Game.h"
+
+Game::Game() {
+    this -> isRunning = false;
+}
+
+Game::~Game() {
+
+}
+
+bool Game::IsRunning() const {
+    return this -> isRunning;
+}
+
+void Game::Initialize(int width, int height) {
+    if(SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cerr << "Error initializing SDL" << std::endl;
+        return;
+    }
+    window = SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_BORDERLESS);
+    if (!window) {
+         std::cerr << "Error creating SDL window" << std::endl;
+        return;
+    }
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if(!renderer) {
+        std::cerr << "Error creating SDL rendererS" << std::endl;
+        return;
+    }
+    isRunning = true;
+    return;
+}
+
+void Game::ProcessInput() {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    switch(event.type) {
+        case SDL_QUIT: {
+            isRunning = false;
+            break;
+        }
+        case SDL_KEYDOWN: {
+            if(event.key.keysym.sym == SDLK_ESCAPE){
+                isRunning = false;
+            }
+        }
+        default: {
+            break;
+        }
+    }
+}
+
+void Game::Update() {
+
+}
+
+void Game::Render() {
+    SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
+    SDL_RenderClear(renderer);
+    SDL_Rect rectangle {
+        (int) PosX,
+        (int) PosY,
+        10,
+        10
+    };
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderFillRect(renderer, &rectangle);
+    SDL_RenderPresent(renderer);
+}
+
+void Game::Destroy() {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_QUIT;
+}
+
