3063bdf63e209d83760e9896f405c55f434fc9ab
David Neyer <neyer.david@googlemail.com>
2020-05-13 12:44:35 +0200

    Add asset and TextureManager

--- src/Entity.h
+++ src/Entity.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <string>
+#include <map>
 
 #include "./EntityManager.h"
 #include "./Component.h"
@@ -15,6 +16,7 @@ class Entity {
         EntityManager& manager;
         bool isActive;
         std::vector<Component*> components;
+        std::map<const std::type_info*, Component*> componentTypeMap;
     public:
         std::string name;
         Entity(EntityManager& manger);
@@ -29,8 +31,13 @@ class Entity {
             T* newComponent(new T(std::forward<TArgs>(args)...));
             newComponent -> owner = this;
             components.emplace_back(newComponent);
+            componentTypeMap[&typeid(*newComponent)] = newComponent;
             newComponent->Initialize();
             return *newComponent;
+        }
+        template <typename T>
+        T* GetComponent() {
+            return static_cast<T*>(componentTypeMap[&typeid(T)]);
         }
 
 };
