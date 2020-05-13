823f5d03c0d123e0e4206cc8c0496a58e5fabc88
David Neyer <neyer.david@googlemail.com>
2020-05-13 18:31:24 +0200

    add sprite component

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build:
-	g++ -w -std=c++14 -Wfatal-errors \
+	g++ -w -g -std=c++14 -Wfatal-errors \
 	./src/*.cpp \
 	-o game \
 	-I"./lib/lua" \
--- src/Game.cpp
+++ src/Game.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include "./Constants.h"
 #include "./Game.h"
+#include "./AssetManager.h"
 #include "./Components/TransformComponent.h"
+#include "./Components/SpriteComponent.h"
 #include "../lib/glm/glm.hpp"
 
 EntityManager manager;
+AssetManager* Game::assetManager = new AssetManager(&manager);
 SDL_Renderer* Game::renderer;
 
 
@@ -43,8 +46,11 @@ void Game::Initialize(int width, int height) {
 }
 
 void Game::LoadLevel(int levelNumber) {
-    Entity& newEntity(manager.AddEntity("projectile"));
+    std::string textureFilePath = "./assets/images/tank-big-right.png";
+    assetManager -> AddTexture("tank-image", textureFilePath.c_str());
+    Entity& newEntity(manager.AddEntity("tank"));
     newEntity.AddComponent<TransformComponent>(0, 0, 20, 20, 32, 32, 1);
+    newEntity.AddComponent<SpriteComponent>("tank-image");
 }
 
 void Game::ProcessInput() {
